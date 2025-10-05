ed8b4d9312e3f7c465c430f87964162377b5c539
Francisco Liu <pancholiu@hotmail.com>
2025-10-04 21:00:47 -0300

    Decorator - 05 Solution (Minor corrections)

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,7 @@
-
-import decorator.CloudStream;
-import decorator.CompressedCloudStream;
-import decorator.EncryptedCloudStream;
-import decorator.Stream;
-import decorator.excercise.Demo;
+import decorator.solution.Demo;
 
 public class Main {
     public static void main(String[] args) {
         new Demo().show();
-
-
-        /*
-        storeCreditCard(new CompressedCloudStream(new CloudStream()));
-         */
-    }
-
-    public static void storeCreditCard(Stream stream) {
-        stream.write("1234-1234-1234-1234");
     }
 }
