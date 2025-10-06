6cffdff5a0943bc9c257ae21b7c386c7cfbf5944
Francisco Liu <francisco.gao@mccain.ca>
2025-10-06 15:59:53 -0300

    Facade - 03 Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import facade.Message;
 import facade.NotificationServer;
+import facade.NotificationService;
 
 public class Main {
     public static void main(String[] args) {
-        var server = new NotificationServer();
-        var connection = server.connect("ip");
-        var authToken = server.authenticate("appId", "key");
-        var message = new Message("Hello world");
-        server.send(authToken, message, "target");
+        var service = new NotificationService();
+        service.send("Hello world", "target");
     }
 }
---
+++ src/facade/NotificationService.java
@@ -0,0 +1,10 @@
+package facade;
+
+public class NotificationService {
+    public void send(String message, String target) {
+        var server = new NotificationServer();
+        var connection = server.connect("ip");
+        var authToken = server.authenticate("appId", "key");
+        server.send(authToken, new Message(message), target);
+    }
+}
