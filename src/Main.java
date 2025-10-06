734e82db1201ae05c09dc68259714f5baa7acd14
Francisco Liu <francisco.gao@mccain.ca>
2025-10-06 15:49:39 -0300

    Facade - 01 the problem

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-import decorator.solution.Demo;
+import facade.Message;
+import facade.NotificationServer;
 
 public class Main {
     public static void main(String[] args) {
-        new Demo().show();
+        var server = new NotificationServer();
+        var connection = server.connect("ip");
+        var authToken = server.authenticate("appId", "key");
+        var message = new Message("Hello world");
+        server.send(authToken, message, "target");
     }
 }
---
+++ src/facade/NotificationServer.java
@@ -0,0 +1,20 @@
+package facade;
+
+public class NotificationServer {
+    // connect() -> Connection
+    // authenticate(appId, Key) -> authToken
+    // send(authToken, message, target)
+    // conn.disconnect()
+
+    public Connection connect(String ipAdress) {
+        return new Connection();
+    }
+
+    public AuthToken authenticate(String appId, String key) {
+        return new AuthToken();
+    }
+
+    public void send(AuthToken authToken, Message message, String target) {
+        System.out.println("Sending a message");
+    }
+}
