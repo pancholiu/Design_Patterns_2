13b9a5335806e0641680e34c5c1065032fda25e6
Francisco Liu <francisco.gao@mccain.ca>
2025-10-09 08:45:55 -0300

    Proxy - 01 The problem

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
-import flyweight.excercise.Demo;
+import proxy.Ebook;
+import proxy.Library;
 
 public class Main {
     public static void main(String[] args) {
-        new Demo().show();
+        var library = new Library();
+        String[] fileNames = { "a", "b", "c" };
+
+        for(var fileName: fileNames)
+            library.add(new Ebook(fileName));
+
+    library.openEbook("a");
+
+
     }
 }
